c5c2508f62145329195dd7e395038398c2fe7c02
Matiss Janis Aboltins <matiss@mja.lv>
2016-11-12 19:13:26 +0200

    More changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -49,10 +49,16 @@ function loadPrice() {
 function showIndividualInfo() {
   $('#insurance_info button[onclick]').addClass('hide');
   $('#individual_info').removeClass('hide');
+    $('html, body').animate({
+        scrollTop: $("#individual_info").offset().top
+    }, 2000);
 }
 function showInsuranceInfo() {
   $('#additional_options button[onclick]').addClass('hide');
   $('#insurance_info').removeClass('hide');
+    $('html, body').animate({
+        scrollTop: $("#insurance_info").offset().top
+    }, 2000);
 }
 
 
@@ -63,16 +69,26 @@ $('#property_type button').on('click', function () {
   $('#property_type button').removeClass('btn-warning');
   $(this).addClass('btn-warning');
   $('#property_size').removeClass('hide');
+
+  $('html, body').animate({
+      scrollTop: $("#property_size").offset().top
+  }, 2000);
 })
 
 
 $('select[name="property_size"]').on('change select', function () {
   if ($(this).val() !== 'Please select') {
     $('#construction_type').removeClass('hide');
+    $('html, body').animate({
+        scrollTop: $("#construction_type").offset().top
+    }, 2000);
   }
 });
 $('select[name="construction_type"]').on('change select', function () {
   if ($(this).val() !== 'Please select') {
     $('#additional_options').removeClass('hide');
+    $('html, body').animate({
+        scrollTop: $("#additional_options").offset().top
+    }, 2000);
   }
 });
